9dadb1a9ff85da18aa9c4c6515ffc1c9b8bdff7d
Charon <ruslan.promouter@gmail.com>
2019-06-17 21:56:34 +0300

    bug fix

--- public_html/inc/db/mysql_utils.php
+++ public_html/inc/db/mysql_utils.php
@@ -4,9 +4,9 @@ function createDbConnection() {
   // TODO: move MySQL credentials to *.ini file
   $connection = mysqli_connect('127.0.0.1', 'root', 'GoticaNeerotice', 'php_blog.db') 
     or die (mysqli_connect_error());
-  mysqli_set_charset($connect, 'utf8');
+  mysqli_set_charset($connection, 'utf8');
 
-  if (!mysqli_ping($connect)) {
+  if (!mysqli_ping($connection)) {
 	die ('Ошибка соединения');
   } 
 
